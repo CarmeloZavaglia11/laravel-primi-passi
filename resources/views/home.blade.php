42d4a298b5dcc9d79fef45da373ac02d235e7442
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-10-08 18:05:06 +0200

    Complete

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@include('partials.header')
+
+    <main>
+        <p>
+            Il Pastificio La Molisana nasce nel 1912, fondato a Campobasso dalla famiglia Carlone come bottega artigianale. Nel 1927 ottiene la Gran Palma d'oro in occasione dell'Esposizione Campionaria di Roma. Nel 1966 il primo logo: quattro spighe di grano che s'intrecciano a fare da sfondo al nome dell'azienda. Il pastificio comincia ad esportare in Europa, Nel 1982 restyling del logo, arricchito dallo stemma di Campobasso.
+
+            Nel 1991 La Molisana inizia la costruzione del Molino, adiacente al pastificio, completando così la filiera produttiva, dalla selezione dei migliori grani alla realizzazione del prodotto finito.
+    
+            Nel 1993 l’azienda sceglie l’attore Nino Manfredi come interprete di una serie di spot firmati da Luca Manfredi: Pasta, Amore & Fantasia. La campagna ha un successo immediato e consacra La Molisana tra i più noti produttori di pasta del mondo. 
+        </p>
+    </main>
+
+@include('partials.footer')  
+
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+    <footer>
+        <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="logo">
+    </footer>
+
+</body>
+</html>
